bd804d92a63233620e0f9300fdfb32ef05002c0e
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-03 21:32:34 +0530

    update: updated the codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,10 @@
-import React, { useEffect, useState } from "react";
-import MemoryGame from "./components/MemoryGame";
+import React from "react";
 import "./App.scss";
 
 const App: React.FC = () => {
-  const [options, setOptions] = useState<any>(0);
-  const [highScore, setHighScore] = useState(0);
-
-  useEffect(() => {
-    // Loads when the game starts
-  }, []);
   return (
     <div>
-      <div className="container">
-        <h1>Memory Game</h1>
-        <div>High Score: {highScore}</div>
-        <div>
-          {options === null ? (
-            <>
-              <button onClick={() => setOptions(12)}>Easy</button>
-              <button onClick={() => setOptions(18)}>Medium</button>
-              <button onClick={() => setOptions(24)}>Hard</button>
-            </>
-          ) : (
-            <>
-              <button
-                onClick={() => {
-                  const prevOptions = options;
-                  setOptions(null);
-                  setTimeout(() => {
-                    setOptions(prevOptions);
-                  }, 5);
-                }}
-              >
-                Start Over
-              </button>
-              <button onClick={() => setOptions(null)}>Main Menu</button>
-            </>
-          )}
-        </div>
-      </div>
-
-      {options ? (
-        <MemoryGame
-          options={options}
-          setOptions={setOptions}
-          highScore={highScore}
-          setHighScore={setHighScore}
-        />
-      ) : (
-        <h2>Choose a difficulty to begin!</h2>
-      )}
+      <h3>App</h3>
     </div>
   );
 };
