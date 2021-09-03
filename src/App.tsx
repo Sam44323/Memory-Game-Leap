855cc2a51139a50501fb7281f65e9f8969f62ea3
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-03 21:57:16 +0530

    add: added react-flip package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 import "./App.scss";
 
+const CONTENT = ["A", "B", "C", "D", "E", "F", "G", "H"]; // a constant for initial content
+
 const App: React.FC = () => {
+  // storing duplicates of alphabets from A...H in an array
+  const [currentContent, setCurrentContent] = React.useState<string[]>([]);
+
+  /**
+   *
+   * @param arr -> string[]
+   * @returns the inputted array shuffled
+   */
+
+  const shuffleArray = (arr: string[]): string[] => {
+    let currentIndex = arr.length;
+    let randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [arr[currentIndex], arr[randomIndex]] = [
+        arr[randomIndex],
+        arr[currentIndex],
+      ];
+    }
+
+    return arr;
+  };
+
+  React.useEffect(() => {
+    setCurrentContent([...shuffleArray(CONTENT), ...shuffleArray(CONTENT)]);
+  }, []);
+
   return (
     <div>
       <h3>App</h3>
